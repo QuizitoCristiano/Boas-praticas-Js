9a5419b3a3df874a37ec925bc17479eddf259e81
quizitocristiano <quizitocristiano10@gmail.com>
2023-11-28 17:37:35 -0300

    add

---
+++ Promises/script.js
@@ -0,0 +1,69 @@
+// function criaNumeroAleatorio(min, max){
+//     min *= 1000;
+//     max *= 1000;
+//     return Math.floor(Math.random() * (max - min) + min);
+// }
+
+// function esperaAi(msg, tempo){
+
+//     return new Promise((resolve, reject) => {
+
+//         if(typeof msg !== 'string') {
+//             reject('BAD VALUE');
+//         }
+
+//         setTimeout( () => {
+//             resolve(msg);
+//         }, tempo);
+
+//     });
+
+// }
+// esperaAi('Conexão como o BD ', criaNumeroAleatorio(1, 4))
+// .then(resposta => {
+//     console.log(resposta);
+//     return esperaAi('Buscando dados da Base', criaNumeroAleatorio(1, 3));
+// }).then(resposta => {
+//     console.log(resposta);
+//     return esperaAi( 2222, criaNumeroAleatorio(1, 3));
+// }).then(resposta => {
+//     console.log(resposta);
+//     return esperaAi('Exibe dados na tela', criaNumeroAleatorio(1,3));
+// }).then(resposta => {
+//  console.log(resposta);
+// })
+// .catch(e => {
+//     console.log('ERRO:', e)
+// });
+
+// esperaAi('Frase 3', criaNumeroAleatorio(1, 4));
+
+function crisNumeroAternativo(min = 1, max = 3) {
+  min *= 1000;
+  max *= 1000;
+
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof msg !== "string") {
+        reject("Isso não é válido:");
+        return;
+      }
+
+      resolve(msg.toUpperCase() + " -  Passei na promise");
+      return;
+    }, tempo);
+  });
+}
+
+esperaAi("Todo deu carto..", crisNumeroAternativo())
+.then((valor) => {
+  console.log(valor);
+  return esperaAi('Opa!! a frese dois deu boa beleza', crisNumeroAternativo())
+}).then(valor => {
+    console.log(valor);
+})
+.catch(e => console.log(e));
